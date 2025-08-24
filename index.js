07d6572edf70ca51bb18562d641e383a19a41142
acertainanimeenjoyer <donamanh2015@gmail.com>
2025-08-24 22:29:34 +0700

    add buildscript

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true, limit: '5mb' }));
-app.use(cors());
+app.use(cors({
+  origin: "https://cardgamefront.vercel.app",  // your Vercel domain
+  methods: ["GET","POST","PUT","PATCH","DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
